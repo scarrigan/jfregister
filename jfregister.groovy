5757a0b1e077a734bbfc4c6f3286dbf27f5b1117
Steve Carrigan <carrigan.steve@gmail.com>
2012-06-14 10:13:54 +0200

    Added file i/o for remembering attended meeting number

--- jfregister.groovy
+++ jfregister.groovy
@@ -1,17 +1,76 @@
 
 
-JForumHelper.meetingNumber
+JForum jforum = new JForum()
+Attendee attendee = new Attendee()
 
-class JForumHelper {
+Integer latestMeetingNumber = jforum.latestMeetingNumber
+if (attendee.latestAttendedMeetingNumber >= latestMeetingNumber) {
+    println "You have already signed up for the latest JForum meeting number " + latestMeetingNumber
+} else {
+    println "You need to sign up for the latest JForum meeting number " + latestMeetingNumber
+    // TODO : Sign up method
+    attendee.latestAttendedMeetingNumber = latestMeetingNumber    
+}
 
-    static Integer getMeetingNumber() {
+
+
+public class Attendee {
+
+    private static final String DATA_FILE = "./meetingnumber.txt"
+    
+    private Integer latestAttendedMeetingNumber
+    
+    public void writeToFile(def data) {
+        File file = new File(DATA_FILE)
+        file.write data.toString()
+    }
+    
+    public String readFromFile() {
+        File file = new File(DATA_FILE) 
+        if (!file.exists()) {
+            return 0;
+        } 
+        file.text
+    }
+    
+    public Integer getLatestAttendedMeetingNumber() {
+       String meetingNumber = readFromFile()
+       if(meetingNumber == null) {
+           return 0    
+       }
+       meetingNumber.toInteger()     
+    }
+    
+    public void setLatestAttendedMeetingNumber(Integer meetingNumber) {
+        latestAttendedMeetingNumber = meetingNumber
+        writeToFile latestAttendedMeetingNumber 
+    }
+}
+
+public class JForum {
+
+    private static final String JFORUM_URL = "http://www.jforum.se"
+    private static final Integer GROUP_POSITION = 1;
+    
+    private String jforumData
+    private Integer latestMeetingNumber
+
+    public JForum() {
+        jforumData = fetchWebpageData()    
+    }
+    
+    private String fetchWebpageData() {
+        new URL(JFORUM_URL).text    
+    }
+
+    public Integer getLatestMeetingNumber() {
         String meetingNumber
-        new URL("http://www.jforum.se").eachLine {
+        jforumData.eachLine {
             (it =~ /.*meeting=(.*?)'/).each{
-               meetingNumber = it[1]
+               meetingNumber = it[GROUP_POSITION]
             }
         }
-        meetingNumber.toInteger()
+        meetingNumber.toInteger()        
     }
 
 }
